13c32519ef0c1fa034acc30b4962d187eb7106a1
bovon <125942069+bovon220@users.noreply.github.com>
2023-07-18 16:00:02 +0700

    Initial commit

---
+++ Bookshop_Bovon/frmLogin.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Bookshop_Bovon
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(454, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ชื่อผู้ใช้";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(454, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "รหัสผ่าน";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textPassword.Location = new System.Drawing.Point(568, 125);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(198, 38);
+            this.textPassword.TabIndex = 3;
+            // 
+            // textUsername
+            // 
+            this.textUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textUsername.Location = new System.Drawing.Point(568, 52);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(198, 38);
+            this.textUsername.TabIndex = 4;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnClose.Image = global::Bookshop_Bovon.Properties.Resources.exit__2_;
+            this.btnClose.Location = new System.Drawing.Point(663, 220);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(103, 107);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "ปิด";
+            this.btnClose.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnLogin.Image = global::Bookshop_Bovon.Properties.Resources.key__1_;
+            this.btnLogin.Location = new System.Drawing.Point(452, 220);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(177, 107);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "เข้าสู่ระบบ";
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Bookshop_Bovon.Properties.Resources.Screenshot_2023_07_18_104349;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 389);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textUsername);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VRU Book Shop";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Bookshop_Bovon/frmLogin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bookshop_Bovon
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection();
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server = .\\sqlexpress;" +
+                "database = dbBookshop; integrated security = true";
+            con.Open();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("ไม่ได้เข้าสู่ระบบภายใน 15 วินาที",
+                "แจ้งเตือน",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            this.Close();   
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //ตรวจสอบการป้อนข้อมูล
+            if(textUsername.Text == "" || textPassword.Text == "")
+            {
+                MessageBox.Show("ป้อนข้อมูลให้ครบ", "ผิดพลาด",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //นำ username กับ password ค้นในตาราง tbemployee
+            //คำสั่ง SQL 
+            string sql = "SELECT * FROM tbEmployee " +
+            //    "WHERE username = '"+textUsername.Text+"' " +
+            //    "AND password= '"+textPassword+"' ";
+            "WHERE username =@username AND " +
+            "password=@password";
+            //ใช้คลาส sqlDataAdapter ส่งคำสั่ง sql ไปประมวลผล
+            SqlDataAdapter da = new SqlDataAdapter(sql,con);
+            da.SelectCommand.Parameters.AddWithValue("username", textUsername.Text);
+            da.SelectCommand.Parameters.AddWithValue("password", textPassword.Text);
+            //เก็บผลลัพธ์ ด้วยคลาส DataTable
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //ตรวจสอบ จำนวนเเถวที่ได้
+            if (dt.Rows.Count > 0)
+                MessageBox.Show("พบข้อมูล" + dt.Rows.Count.ToString() + "รายการ");
+            else 
+                MessageBox.Show("ไม่พบข้อมูล");
+        }
+    }
+}
